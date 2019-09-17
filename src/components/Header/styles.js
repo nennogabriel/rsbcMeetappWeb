bdee53e3640eee79dd93e5e024aeb34120edf663
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-17 17:02:22 -0300

    feat: logout

---
+++ src/components/Header/styles.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: rgba(0, 0, 0, 0.3);
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-radius: 1px solid #eee;
+    }
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  padding-left: 20px;
+  font-size: 1.3rem;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #eee;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 1.2rem;
+      color: #aaa;
+    }
+  }
+
+  button {
+    background: #d44059;
+    border-radius: 4px;
+    color: #fff;
+    padding: 10px 20px;
+    font-size: 1.4rem;
+    font-weight: bold;
+  }
+`;
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
+import { color } from '~/styles';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  min-height: 100%;
+  background: linear-gradient(0deg, ${color.bgStart}, ${color.bgEnd});
+  color: #fff;
+`;
