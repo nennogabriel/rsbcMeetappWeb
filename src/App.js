e8fff89a7f9323ae025f49f8fb76eccd64788fac
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-16 23:37:49 -0300

    copy reactotron and redux-saga structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
-// import { Container } from './styles';
+import '~/config/ReactotronConfig';
 
-export default function App() {
-  return <div>Hello</div>;
+import Routes from '~/routes';
+import { store, persistor } from '~/store';
+import history from '~/services/history';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 }
+
+export default App;
